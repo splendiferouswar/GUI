65a0f3f93f22db4bca8bc8cbc793a0f5ad0f2baa
Manesh Harish Ramji <maneshharish@gmail.com>
2020-09-18 15:03:12 +1200

    Add project files.

---
+++ GUI/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.firstNumber = new System.Windows.Forms.TextBox();
+            this.secondNumber = new System.Windows.Forms.TextBox();
+            this.primesBox = new System.Windows.Forms.TextBox();
+            this.primeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(579, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "GO!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(121, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "First Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(125, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Second Number";
+            // 
+            // firstNumber
+            // 
+            this.firstNumber.Location = new System.Drawing.Point(290, 83);
+            this.firstNumber.Name = "firstNumber";
+            this.firstNumber.Size = new System.Drawing.Size(269, 22);
+            this.firstNumber.TabIndex = 3;
+            // 
+            // secondNumber
+            // 
+            this.secondNumber.Location = new System.Drawing.Point(289, 132);
+            this.secondNumber.Name = "secondNumber";
+            this.secondNumber.Size = new System.Drawing.Size(270, 22);
+            this.secondNumber.TabIndex = 4;
+            // 
+            // primesBox
+            // 
+            this.primesBox.Location = new System.Drawing.Point(287, 199);
+            this.primesBox.Multiline = true;
+            this.primesBox.Name = "primesBox";
+            this.primesBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.primesBox.Size = new System.Drawing.Size(272, 196);
+            this.primesBox.TabIndex = 5;
+            // 
+            // primeLabel
+            // 
+            this.primeLabel.AutoSize = true;
+            this.primeLabel.Location = new System.Drawing.Point(126, 202);
+            this.primeLabel.Name = "primeLabel";
+            this.primeLabel.Size = new System.Drawing.Size(51, 17);
+            this.primeLabel.TabIndex = 6;
+            this.primeLabel.Text = "Primes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.primeLabel);
+            this.Controls.Add(this.primesBox);
+            this.Controls.Add(this.secondNumber);
+            this.Controls.Add(this.firstNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox firstNumber;
+        private System.Windows.Forms.TextBox secondNumber;
+        private System.Windows.Forms.TextBox primesBox;
+        private System.Windows.Forms.Label primeLabel;
+    }
+}
+
---
+++ GUI/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1, num2, tempNo;
+
+            num1 = Int32.Parse(firstNumber.Text); //take the first number from user input and put it into num1 variable
+            num2 = Int32.Parse(secondNumber.Text); //take the seconf number from the user and put it inside the num2 variable
+
+            if (num1 > num2) //if the first number entered is bigger than the second number then do the following
+            {
+                tempNo = num1; //take the first (the big one) put inside a tamporary variable called tempNo
+                num1 = num2; //take teh big number and put it inside the num2 variable
+                num2 = tempNo; //take the temporary small number and give it to num2
+            }
+
+            goThrough(num1, num2);
+        }
+        public static bool isPrime(int number) //a boolean function to check if a number is a prime
+        {
+            bool primeBool = true; //creating a boolean variable to keep track of whether a number is prime or not
+
+            if (number < 2) primeBool = false; //if the number is less than 2 return a false
+            int sqrRoot = (int)Math.Sqrt((double)number); //finding the square root of the number
+            for (int i = 2; i <= sqrRoot; i += 1) //start with two, keep moving 2 numbers up, stop until you reach the square root of the number
+            {
+                if (number % i == 0) primeBool = false; //if the modulo is 0 (if after dividing, the remainder is zero), return false
+            }
+            return primeBool; //otherwise return true, only prime numbers will create a true
+        }
+        public void goThrough(int num1, int num2) //function for going through every number within the range
+        {
+            int counter = 0; //counter to keep track for when to put in a new line
+            for (int i = num1; i <= num2; i++) //start the loop  with the small number, keep adding 1 untill the num2
+            {
+                if (isPrime(i)) //if it is a prime 
+                {
+                    primesBox.Text += i.ToString() + ", "; //append to the text box and add a coma
+                    counter++; //increase the counter by 1
+                }
+                if (counter == 5) //if the counter has reached 5
+                {
+                    primesBox.Text += Environment.NewLine; //add an empty line
+                    counter = 0; //reset the counter
+                }
+            }
+        }
+    }
+}
